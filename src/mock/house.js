c4510868dfedbc532af1df6991121eaf42ea9055
FBek90 <f.f.rustamov@bk.ru>
2021-06-14 22:45:25 +0900

    template

---
+++ src/mock/house.js
@@ -0,0 +1,23 @@
+const data = [
+    {
+        id: 1,
+        title: 'Brand New House',
+        location: {long:'412.2231', lat: '123'},
+        category: 'house',
+        price: '100000',
+        sale: '20000',
+        likeCount: 12,
+        owner: {
+            name: 'Owner Name',
+            img: 'img url',
+            contact: ''
+        },
+        facilities: {
+            bed: '2ta',
+            garage: '1ta',
+            bath: '2ta',
+            sqr: '122m'
+        } 
+
+    }
+]
